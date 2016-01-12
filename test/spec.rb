e751db81378b9fcce0aba3fe9a6835c2bba575c4
Brenton Earl <brent@exitstatusone.com>
2016-01-12 10:30:42 -0700

    Added spec file for testing

---
+++ test/spec.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/benchmark'
+
+
+class SlackRequired
+  def initialize
+  end
+end
+
+describe "SlackRequired" do
+
+  before do
+    @sr = SlackRequired.new
+  end
+end
